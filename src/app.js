a682f88b1513138e97c696e17a376f7ae9624ba3
ismailmurat korkmaz <348887@ogr.ktu.edu.tr>
2021-10-19 15:36:37 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array - dizi
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+//length- uzunluğu kaçtane eleman var 
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("<ul>")
+
+
+
+console.log(konutKredileri)
